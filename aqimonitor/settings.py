1402f57e927f047d0e503ea6f46d93100a68059b
Rony <ronyrock1@gmail.com>
2021-08-20 17:37:47 +0530

    -

--- aqimonitor/settings.py
+++ aqimonitor/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l7*urvm&hi6kl1u5*p95xek=g01-vq(y4e%s7cb@(@w2*sxa2c'
+#SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
